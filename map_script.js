9cdc3eb6c75636ebec5862f72086ac00b20525e3
praneethd7 <sd37@illinois.edu>
2022-03-26 18:38:26 -0500

    midpoint popup

--- map_script.js
+++ map_script.js
@@ -27,9 +27,19 @@ fetchJSON('portland_gtfs.geojson') // portland_gtfs
         data.features.forEach(function (feature) {
             // console.log(feature);
             // var symbol = feature.properties['icon'];
-            feature.properties['width'] = Math.max(feature.properties['Traversals'] / max_tr * 12, 1)
+            feature.properties['width'] = Math.max(feature.properties['Traversals'] / max_tr * 12, 3)
             feature.properties['stop_id1'] = feature.geometry.coordinates[0]
             feature.properties['stop_id2'] = feature.geometry.coordinates.slice(-1)[0]
+            l = feature.geometry.coordinates.length
+            if (l == 2){
+                mp = (feature.geometry.coordinates[0] + feature.geometry.coordinates[1])/2
+                feature.properties['mp_lng'] = mp[0]
+                feature.properties['mp_lat'] = mp[1]
+            }else{
+                mp = feature.geometry.coordinates.slice(parseInt(l/2))[0]
+                feature.properties['mp_lng'] = mp[0]
+                feature.properties['mp_lat'] = mp[1]
+            }
             data_list.push(feature)
             stop_circles_list.push({
                 "type": "Feature",
@@ -42,18 +52,21 @@ fetchJSON('portland_gtfs.geojson') // portland_gtfs
                 "type": "Feature",
                 "geometry": {
                     "type": "Point",
-                    "coordinates": feature.geometry.coordinates.slice(-1)[0]
+                    "coordinates":  feature.geometry.coordinates.slice(-1)[0]
                 }
             })
         });
     });
+console.log(stop_circles_list.length)
+stop_circles_list = [... new Set(stop_circles_list)]
+console.log(stop_circles_list.length)
 routes_inp = {
     "type": "FeatureCollection",
     "features": data_list
 }
 stops_inp = {
     "type": "FeatureCollection",
-    "features": data_list
+    "features": stop_circles_list
 }
 
 console.log(routes_inp)
@@ -128,8 +141,8 @@ map.on('load', () => {
                 ]
             ],
             'circle-stroke-color': '#B2222F',
-            'circle-opacity': 0.25,
-            'circle-stroke-opacity': 0.35
+            'circle-opacity': 0.15,
+            'circle-stroke-opacity': 0.25
         }
     }, 0);
 
@@ -182,23 +195,24 @@ map.on('load', () => {
     map.on('mousemove', 'route', (e) => {
         // Display Prop
         display_prop(e)
-        console.log(e.features)
         // Change the cursor style as a UI indicator.
         map.getCanvas().style.cursor = 'pointer';
 
         // Copy coordinates array.
-        const coordinates = e.features[0].geometry.coordinates.slice();
+        const coordinates = e.features[0].geometry.coordinates;
         const traversals = e.features[0].properties.Traversals;
         const segment = e.features[0].properties.segment_id;
         // Ensure that if the map is zoomed out such that multiple
         // copies of the feature are visible, the popup appears
         // over the copy being pointed to.
-        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        midpoint = [e.features[0].properties.mp_lng,e.features[0].properties.mp_lat]
+        // console.log(turf.along(e.features[0],turf.length(e.features[0])/2))
+        while (Math.abs(e.lngLat.lng - midpoint) > 180) {
+            midpoint += e.lngLat.lng > midpoint ? 360 : -360;
         }
         // Populate the popup and set its coordinates
         // based on the feature found.
-        popup.setLngLat(coordinates[0]).setHTML(
+        popup.setLngLat(midpoint).setHTML(
             "<p>Segment ID: " + segment + "</p>" +
             "<p>Traversals: " + traversals + "</p>"
         ).addTo(map);
